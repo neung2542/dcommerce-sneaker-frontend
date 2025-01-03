84b8ec75bebdcd1affda4e696f2b4a3df3a5f79a
Tyler <jakarinp1999@gmail.com>
2025-01-03 14:17:43 +0700

    Update AnimateLoading.tsx

--- src/app/ui/AnimateLoading.tsx
+++ src/app/ui/AnimateLoading.tsx
@@ -37,12 +37,12 @@ const SkeletonCard = () => {
   );
 };
 
-const LoadingSpinner = () => {
-  return (
-    <div className="flex justify-center items-center h-full">
-      <div className="loader ease-linear rounded-full border-4 border-t-4 border-black h-12 w-12"></div>
-    </div>
-  );
-};
+// const LoadingSpinner = () => {
+//   return (
+//     <div className="flex justify-center items-center h-full">
+//       <div className="loader ease-linear rounded-full border-4 border-t-4 border-black h-12 w-12"></div>
+//     </div>
+//   );
+// };
 
 export default AnimateLoading;
